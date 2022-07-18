bd29acae167457571ffad13b5bd312332aefab1f
yupeng <yupeng9437@qq.com>
2022-07-18 19:37:22 +0800

    搭建 payment 和 consumer 服务

---
+++ cloud-provide-payment/src/main/java/com/study/springcloud/service/PaymentImpl.java
@@ -0,0 +1,21 @@
+package com.study.springcloud.service;
+
+import com.study.springcloud.pojo.Payment;
+import com.study.springcloud.dao.PaymentDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentImpl implements PaymentService {
+    @Autowired
+    PaymentDao paymentDao;
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment queryById(long id) {
+        return paymentDao.queryById(id);
+    }
+}
