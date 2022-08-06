e0e2fc4cd65c5a3b0235b3b01ed29f8594b31bca
paul <507435917@qq.com>
2022-08-06 17:30:10 +0800

    自定义登录成功/失败 handler

--- spring-security-demo/src/main/java/com/study/springcloud/config/SecurityConfig.java
+++ spring-security-demo/src/main/java/com/study/springcloud/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.study.springcloud.config;
 
+import com.study.springcloud.handler.MyAuthenticationFailureHandler;
+import com.study.springcloud.handler.MyAuthenticationSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,14 +21,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // 表单提交
         http.formLogin()
+                // 自定义 username 参数名, password 参数名
+                .usernameParameter("username123")
+                .passwordParameter("password123")
                 // 自定义登录页面
                 .loginPage("/login.html")
                 // 必须和表单提交的接口（位于 login.html 中）一样，回去执行自定义登录逻辑
                 .loginProcessingUrl("/login")
                 // 登录成功后跳转的页面, POST 请求
-                .successForwardUrl("/toMain")
+//                .successForwardUrl("/toMain")
+                // 自定义 success handler
+                .successHandler(new MyAuthenticationSuccessHandler("https://www.baidu.com"))
                 // 登录失败页面跳转
-                .failureForwardUrl("/toError");
+//                .failureForwardUrl("/toError");
+                .failureHandler(new MyAuthenticationFailureHandler("https://www.sina.com.cn"));
 
         // 授权
         http.authorizeRequests()
