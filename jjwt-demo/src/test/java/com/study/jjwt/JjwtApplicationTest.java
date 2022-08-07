debb06b1b158682e9fb0f612aa754b7c986fdb7e
paul <507435917@qq.com>
2022-08-07 22:33:18 +0800

    jjwt parse token

--- jjwt-demo/src/test/java/com/study/jjwt/JjwtApplicationTest.java
+++ jjwt-demo/src/test/java/com/study/jjwt/JjwtApplicationTest.java
@@ -1,5 +1,6 @@
 package com.study.jjwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -39,6 +40,24 @@ public class JjwtApplicationTest {
         System.out.println(Base64Codec.BASE64.decodeToString(split[1]));
         // 签名部分会乱码
         System.out.println(Base64Codec.BASE64.decodeToString(split[2]));
+    }
+
+    // 解析 token
+    @Test
+    public void testParseToken() {
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4ODgiLCJzdWIiOiJSb3NlIiwiaWF0IjoxNjU5ODgyMzk3fQ.iu1SSOio2IOM5sUkWHgNTktVFzqupuMt1ky8GSDvhes";
+
+        // 解析 token，获取 Claims 对象，jwt 中声明的荷载对象
+        Claims claims = (Claims) Jwts.parser()
+                // 密钥，这个密钥要跟 token 签发时的密钥保持一致
+                .setSigningKey("salt_secret_xxxx")
+                .parse(token)
+                .getBody();
+
+        System.out.println("id=" + claims.getId());
+        System.out.println("sub=" + claims.getSubject());
+        System.out.println("iat=" + claims.getIssuedAt());
+
     }
 
 }
