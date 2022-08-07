0a4321fd58fb884cceb138b17cef67e615c6eb7a
paul <507435917@qq.com>
2022-08-07 23:53:38 +0800

    jwt token 在服务器端解析

--- spring-security-oauth2-demo/src/main/java/com/study/springcloud/controller/UserController.java
+++ spring-security-oauth2-demo/src/main/java/com/study/springcloud/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.study.springcloud.controller;
 
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/user")
@@ -19,6 +23,24 @@ public class UserController {
 
         // 本质就是返回 user information , 也就是我们在 pojo 包中定义的 User 对象
         return authentication.getPrincipal();
+    }
+
+    /**
+     * 在服务器端解析 jwt token
+     * @param authentication
+     * @return
+     */
+    @RequestMapping("/getJwt")
+    public Object getCurrentUser(Authentication authentication, HttpServletRequest request) {
+
+        // 解析 jwt token
+        String header = request.getHeader("Authorization");
+        String token = header.substring(header.lastIndexOf("bearer") + 7);
+
+        return Jwts.parser()
+                .setSigningKey("test_secret_key".getBytes(StandardCharsets.UTF_8))
+                .parseClaimsJws(token)
+                .getBody();
     }
 
 }
